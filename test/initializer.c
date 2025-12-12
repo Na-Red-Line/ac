57934800f9bf32587a5b6b29750593ea50b818d2
Relsola <sola001018@gmail.com>
2025-12-12 23:24:14 +0800

    update

--- test/initializer.c
+++ test/initializer.c
@@ -19,6 +19,23 @@ int main() {
   ASSERT(0, ({ int x[2][3]={{1,2}}; x[1][0]; }));
   ASSERT(0, ({ int x[2][3]={{1,2}}; x[1][2]; }));
 
+  ASSERT('a', ({ char x[4]="abc"; x[0]; }));
+  ASSERT('c', ({ char x[4]="abc"; x[2]; }));
+  ASSERT(0, ({ char x[4]="abc"; x[3]; }));
+  ASSERT('a', ({ char x[2][4]={"abc","def"}; x[0][0]; }));
+  ASSERT(0, ({ char x[2][4]={"abc","def"}; x[0][3]; }));
+  ASSERT('d', ({ char x[2][4]={"abc","def"}; x[1][0]; }));
+  ASSERT('f', ({ char x[2][4]={"abc","def"}; x[1][2]; }));
+
+  ASSERT(4, ({ int x[]={1,2,3,4}; x[3]; }));
+  ASSERT(16, ({ int x[]={1,2,3,4}; sizeof(x); }));
+  ASSERT(4, ({ char x[]="foo"; sizeof(x); }));
+
+  ASSERT(4, ({ typedef char T[]; T x="foo"; T y="x"; sizeof(x); }));
+  ASSERT(2, ({ typedef char T[]; T x="foo"; T y="x"; sizeof(y); }));
+  ASSERT(2, ({ typedef char T[]; T x="x"; T y="foo"; sizeof(x); }));
+  ASSERT(4, ({ typedef char T[]; T x="x"; T y="foo"; sizeof(y); }));
+
   printf("OK\n");
   return 0;
 }
